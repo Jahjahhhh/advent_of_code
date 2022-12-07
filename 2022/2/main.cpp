455db400883ccc6ac5a33befc0161dce77c43318
Jahjahhhh <xjiapeng@gmail.com>
2022-12-07 02:42:18 -0500

    man made horrors beyond your comprehension

--- 2022/2/main.cpp
+++ 2022/2/main.cpp
@@ -6,8 +6,42 @@
 
 using namespace std;
 
-int main(void) {
- 
+int partOne();
+
+// part 2 of the puzzle
+int main(void) { 
+    // Uncomment line 14 to run part 1 of the puzzle
+    // partOne();
+    ifstream input("input.txt");
+    char them;
+    char me;
+    vector<char> theirMoves;
+    vector<char> myMoves;
+    int myPoints = 0;
+
+    while(input >> them >> me) {
+        theirMoves.push_back(them);
+        myMoves.push_back(me);
+    }
+    // A = ROCK (1)        X = LOSS (0)    ROCK (1)
+    // B = PAPER (2)       Y = DRAW (3)    PAPER (2)
+    // C = SCISSORS (3)    Z = WIN (6)     SCISSORS (3)
+    for(int i = 0; i < myMoves.size(); i++) {
+        switch(theirMoves[i]) {
+            case 'A': if(myMoves[i] == 'Y') myPoints += 4; else if(myMoves[i] == 'X') myPoints += 3; else if(myMoves[i] == 'Z') myPoints += 8; break;
+            case 'B': if(myMoves[i] == 'Y') myPoints += 5; else if(myMoves[i] == 'X') myPoints += 1; else if(myMoves[i] == 'Z') myPoints += 9; break;
+            case 'C': if(myMoves[i] == 'Y') myPoints += 6; else if(myMoves[i] == 'X') myPoints += 2; else if(myMoves[i] == 'Z') myPoints += 7; break;
+            default: cout << "wrong input\n";
+        }
+    }
+
+    cout << myPoints;
+
+    return 0;
+}
+
+int partOne(void) {
+    
     ifstream input("input.txt");
     char them;
     char me;
@@ -22,8 +56,6 @@ int main(void) {
     // A = ROCK (1)        X = ROCK (1)         VIC ROYALE = 6
     // B = PAPER (2)       Y = PAPER (2)        L          = 0
     // C = SCISSORS (3)    Z = SCISSORS (3)     TIE        = 3
-
-
     for(int i = 0; i < myMoves.size(); i++) {
         switch(theirMoves[i]) {
             case 'A': if(myMoves[i] == 'Y') myPoints += 8; else if(myMoves[i] == 'X') myPoints += 4; else if(myMoves[i] == 'Z') myPoints += 3; break;
