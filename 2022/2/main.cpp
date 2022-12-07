78cfc42969d37393edad84931519b0b3299f7014
Jahjahhhh <xjiapeng@gmail.com>
2022-12-07 02:28:44 -0500

    man made horrors beyond your comprehension

---
+++ 2022/2/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+ 
+    ifstream input("input.txt");
+    char them;
+    char me;
+    vector<char> theirMoves;
+    vector<char> myMoves;
+    int myPoints = 0;
+
+    while(input >> them >> me) {
+        theirMoves.push_back(them);
+        myMoves.push_back(me);
+    }
+    // A = ROCK (1)        X = ROCK (1)         VIC ROYALE = 6
+    // B = PAPER (2)       Y = PAPER (2)        L          = 0
+    // C = SCISSORS (3)    Z = SCISSORS (3)     TIE        = 3
+
+
+    for(int i = 0; i < myMoves.size(); i++) {
+        switch(theirMoves[i]) {
+            case 'A': if(myMoves[i] == 'Y') myPoints += 8; else if(myMoves[i] == 'X') myPoints += 4; else if(myMoves[i] == 'Z') myPoints += 3; break;
+            case 'B': if(myMoves[i] == 'Y') myPoints += 5; else if(myMoves[i] == 'X') myPoints += 1; else if(myMoves[i] == 'Z') myPoints += 9; break;
+            case 'C': if(myMoves[i] == 'Y') myPoints += 2; else if(myMoves[i] == 'X') myPoints += 7; else if(myMoves[i] == 'Z') myPoints += 6; break;
+            default: cout << "wrong input\n";
+        }
+    }
+
+    cout << myPoints;
+
+    return 0;
+}
